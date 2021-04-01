7a523b53082947b3caf9ce3255ed5970701e8e8c
Creative-Lee <77152650+Creative-Lee@users.noreply.github.com>
2021-04-01 21:47:25 +0900

    21.04.01
    해치웠나 ?

--- 연습실/3차(타이머).js
+++ 연습실/3차(타이머).js
@@ -7,26 +7,29 @@ window.onload = function(){
         clickButton.style.height = "100px"                      
         clickButton.value = 'click fast!'
         clickButton.style.gridArea = 'click'+ Math.round((Math.random()*24));
-    })
+    });
 
     const score = document.getElementById("score");
     let clickCounter = 0;
     clickButton.addEventListener('click' , function(){
         clickCounter += 1;
-        score.innerHTML = 'go fast! →  '+clickCounter+'/'+5;
-    })
-
-    const timer = document.getElementById("timer");
-    let timeStart = 0;
-    clickButton.addEventListener("click", function(){
-       var ss = setInterval(function(){
-            if(score.innerHTML == 'go fast! →  '+5+'/'+5){
-                clearInterval(ss);
-                alert('game over!'+'<br>'+'your record is '+timeStart/10+'seconds!')
-            }
-            timer.innerHTML ='time is gold! →  ' + timeStart/10+'s';
-            timeStart += 1;    
-            }, 100)    
+        score.innerHTML = 'go fast! →  '+clickCounter+'/'+25;
     });
-}
 
+    const timer = document.getElementById("timer"); 
+    let timeStart = 0;
+    clickButton.addEventListener("focus", function(){
+       var ss = setInterval(function(){
+            if(score.innerHTML == 'go fast! →  '+25+'/'+25){
+                clearInterval(ss);
+                alert('game over!\n your record is ['+timeStart/100+'] seconds!')
+            }
+            timer.innerHTML ='Time is gold! →  ' + timeStart/100+'s';
+            timeStart += 1;    
+            }, 10);    
+    });
+
+    var getName =  prompt("사용자 이름을 적어주세요!",);
+    var userName =  document.getElementById("nameArea");
+    userName.innerHTML = 'hello! '+getName ;
+ }
