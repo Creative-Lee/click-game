915c6191c3d6e8aefaa96959e2f4489097016ea6
Creative-Lee <77152650+Creative-Lee@users.noreply.github.com>
2021-04-07 00:31:01 +0900

    21.04.07
    타이머가 문제다.

--- 연습실/리팩토링.js
+++ 연습실/리팩토링.js
@@ -16,27 +16,37 @@ window.onload = function(){
 
     const count = document.getElementById("countReturn")    // 카운터 h1
     var c = 0;
-
+    
     function buttonClick(){
         if(gameStartEnd() == false){ //시작전이면
-            movingButton.style.gridArea = 'click'+ Math.round((Math.random()*24));  //버튼 움직임!!
-
-            var timer = setInterval(function(){  // 타이머!!
+            game = 1;       // 1==true 진행중으로 바꿔
+            movingButton.style.gridArea = 'click'+ Math.round((Math.random()*24));  //첫 버튼 움직임!! 
+            
+            setInterval(function(){  // 타이머!!
                 timerReturn.innerHTML = t/10;    
                 t += 1 ;
                 },100)
 
-            count.innerHTML = c;
-            c += 1;
-            
-            game = 1;       // 1==true 진행중으로 바꿔
+            c += 1;               //카운터
+            count.innerHTML = c + "/10" ; 
         }
-        else if(gameStartEnd() == true){  //진행중이면
-            movingButton.style.gridArea = 'click'+ Math.round((Math.random()*24));  //버튼 움직임
-
-            count.innerHTML = c;
+        else if(gameStartEnd() == true) {  //진행중이면
+            movingButton.style.gridArea = 'click'+ Math.round((Math.random()*24));  //이후 버튼 움직임
             c += 1;
+            count.innerHTML = c + "/10" ;  //카운터 2~10
         }
     }
     movingButton.addEventListener('click' , buttonClick);   // 버튼을 클릭하면 buttonClick 함수 실행
+   
 }
+
+    //function gameReset(){
+    //    if(c == 10){
+    //        timer = clearInterval(timer);
+    //        alert("your record --> " + timerReturn.innerHTML + "s");
+    //        game = 0;
+    //        c = 0;
+    //        timerReturn.innerHTML = "↓ ↓ ↓"
+    //        count.innerHTML = "focus on!"
+    //    }
+    //}
