182843d734eef4ba68fbcae292025a141e2e46b0
Creative-Lee <77152650+Creative-Lee@users.noreply.github.com>
2021-04-08 13:12:56 +0900

    21.04.08
    분기 2개로 줄임 + 웹킷 달아봄 + 더블클릭 확대 방지

--- 연습실/리팩토링.js
+++ 연습실/리팩토링.js
@@ -1,5 +1,6 @@
 window.onload = function(){
-    var isStart;                //  게임상태 변수
+
+    let isStart;                //  게임상태 변수
     function gameStartEnd(){    //  게임상태에 따라 불리언 값 리턴
         return isStart          
     }
@@ -7,12 +8,13 @@ window.onload = function(){
     const movingButton = document.getElementById("catch");  // 버튼 input
     
     const count = document.getElementById("countReturn")    // 카운터 html
-    var c; //카운터 변수
+    let c; //카운터 변수
 
     const timerReturn = document.getElementById("timerReturn"); //타이머 html
-    var t;  // 타이머 변수
-    var timer;  // setInterval(function(){})를 담을거임  clearInterval 시발놈 이거때매 알게됨 
+    let t;  // 타이머 변수
+    let timer;  // setInterval(function(){})를 담을거임  clearInterval 시발놈 이거때매 알게됨 
 
+    //초기화 로직 
     function initialize(){
         isStart = false;                                // 게임 시작전으로
 
@@ -25,27 +27,25 @@ window.onload = function(){
         
         movingButton.style.gridArea = 'click' + 12;      // 버튼 원위치 리셋
     }
-    
+    //초기화 로직 끝
+
     function buttonClick(){
         c += 1;  // 매 회 클릭마다 카운터 변수는 조건없이 바로 +1
-        if(!gameStartEnd()){ // 시작전이면
-            isStart = true;   // 게임 진행중으로
-            movingButton.style.gridArea = 'click'+ Math.round((Math.random()*24));  //첫 버튼 움직임!!    
 
-            timer = setInterval(function(){  // 타이머!!
+        if(!gameStartEnd() || (gameStartEnd() && c < 10)){ // 시작전이거나 , 진행중이면서 카운트가 10 이하면
+            if(!gameStartEnd()){    // 시작전이면 
+                timer = setInterval(function(){  // 타이머!!
                 timerReturn.innerHTML = t/100;    
                 t += 1 ;
                 },10)
+            
+                isStart = true;   // 게임 진행중으로
+            }
+            movingButton.style.gridArea = 'click'+ Math.round((Math.random()*24));  //첫 버튼 움직임!!    
 
             count.innerHTML = c + "/10" ;  //카운터 1        -- 게임set의 기준을 바꾸려면 이것 숫자를 바꿔야함
         }
-        else if(gameStartEnd() && c < 10) {  //진행중이면 and 카운터가 10까지   -- 게임set의 기준을 바꾸려면 이것 비교 연산자(<) 우항 숫자를 바꿔야함
-            movingButton.style.gridArea = 'click'+ Math.round((Math.random()*24));  //버튼 움직임 2~10
-
-            count.innerHTML = c + "/10" ;  //카운터 2~10    -- 게임set의 기준을 바꾸려면 이것 숫자를 바꿔야함
-        }
-        else if(gameStartEnd() && c == 10){  //진행중이면 and 카운터가 10에 도달했을 때  -- 게임set의 기준을 바꾸려면 이것 동등연산자(==) 우항 숫자를 바꿔야함
-
+        else{
             alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \n아직 멀었군요^^");  //타이머 멈춘 시점  알람 출력
             initialize();
         }
