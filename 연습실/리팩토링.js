43a0acf5cd1d11d84ce7a9b331cd6228104bce40
Creative-Lee <77152650+Creative-Lee@users.noreply.github.com>
2021-04-10 19:54:23 +0900

    21.04.10
    이름 받아오기 로직 수정

--- 연습실/리팩토링.js
+++ 연습실/리팩토링.js
@@ -1,10 +1,10 @@
 window.onload = function(){
-
+    
     let isStart;                //  게임상태 변수
     function gameStartEnd(){    //  게임상태에 따라 불리언 값 리턴
         return isStart          
     }
-
+    
     const movingButton = document.getElementById("catch");  // 버튼 input
     
     const count = document.getElementById("countReturn")    // 카운터 html
@@ -13,6 +13,7 @@ window.onload = function(){
     const timerReturn = document.getElementById("timerReturn"); //타이머 html
     let t;  // 타이머 변수
     let timer;  // setInterval(function(){})를 담을거임  clearInterval 시발놈 이거때매 알게됨 
+
 
     //초기화 로직 
     function initialize(){
@@ -29,6 +30,7 @@ window.onload = function(){
     }
     //초기화 로직 끝
 
+    //버튼 클릭 로직 
     function buttonClick(){
         c += 1;  // 매 회 클릭마다 카운터 변수는 조건없이 바로 +1
 
@@ -47,27 +49,23 @@ window.onload = function(){
         }
         else{
             result();
-            //alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \n아직 멀었군요^^");  //타이머 멈춘 시점  알람 출력
             initialize();
-
         }
     }
+    //버튼 클릭 로직 끝
 
+    //사용자 이름 받아오기 로직 
     function getName(){
         const nameArea = document.getElementById("nameArea");                       // 이름 html
-        nameArea.innerHTML =  "hello!  " + prompt("당신의 이름을 적어주세요!",);      // 이름값 받는 prompt
-//if(nameArea.innerHTML == "hello!  " + ""  || nameArea.innerHTML == "hello!  " + null){
-//    for(i=100){
-//
-//    }
-//        nameArea.innerHTML =  "hello!  " + prompt("좋게 말할 때 적어주세요^^",);
-//    
-//
-//}
-//
-//
-    }
+        nameArea.innerHTML = "hello!  " + prompt("당신의 이름을 적어주세요!",);              // 이름값 받는 prompt
 
+        while(nameArea.innerHTML === "hello!  " + "" || nameArea.innerHTML === "hello!  "  + null){  //받은 값이 빈 문자열이거나 null 이면
+            nameArea.innerHTML = "hello!  " + prompt("좋은 말 할 때 적어주세요^^",);                   // prompt 무한 실행
+        }
+    }
+    //사용자 이름 받아오기 로직 끝 
+
+    //결과창 로직
     function result(){
         let grade = timerReturn.innerHTML
         let gradeReturn;
@@ -87,8 +85,8 @@ window.onload = function(){
         else if((3 <= grade) && (grade < 4)){
             gradeReturn = "3초"
         }
-        else if((2 <= grade) && (grade < 3)){
-            gradeReturn = "2초"
+        else if(grade < 3){
+            gradeReturn = "3초미만"
         }
         switch(gradeReturn){
             case "7초이상" : alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \n아직 멀었군요^^");  //타이머 멈춘 시점  알람 출력
@@ -101,15 +99,16 @@ window.onload = function(){
             break;
             case "3초" : alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \n인정합니다^^");  //타이머 멈춘 시점  알람 출력
             break;
-            case "2초" : alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \nGOD " +nameArea.innerHTML );  //타이머 멈춘 시점  알람 출력
+            case "3초미만" : alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \nGOD " +nameArea.innerHTML );  //타이머 멈춘 시점  알람 출력
             break;
         }
-    }
+    }   
+    //결과창 로직 끝
 
-
-    initialize();
-    getName();
-    movingButton.addEventListener('click' , buttonClick);   // 버튼을 클릭하면 buttonClick 함수 실행
-    
+    //실행
+    initialize(); // 초기화하고 
+    getName();   // 이름을 받아오고 
+    movingButton.addEventListener('click' , buttonClick);   // 버튼 클릭과 함께 게임을 시작함
+    //실행 끝
 }
 
