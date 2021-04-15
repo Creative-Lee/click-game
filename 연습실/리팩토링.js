633e22e67dd52dcb650679d39e1fb8fda8620275
Creative-Lee <77152650+Creative-Lee@users.noreply.github.com>
2021-04-15 21:34:08 +0900

    21.04.15
    해치웠나..?

--- 연습실/리팩토링.js
+++ 연습실/리팩토링.js
@@ -1,10 +1,13 @@
 window.onload = function(){
     
-    let isStart;                //  게임상태 변수
-    function gameStartEnd(){    //  게임상태에 따라 불리언 값 리턴
+    // 게임상태 로직
+    let isStart;               
+    function gameStartEnd(){    
         return isStart          
     }
-    
+    //게임상태 로직 끝
+
+    // 상수 변수 
     const movingButton = document.getElementById("catch");  // 버튼 input
     
     const count = document.getElementById("countReturn")    // 카운터 html
@@ -14,6 +17,9 @@ window.onload = function(){
     let t;  // 타이머 변수
     let timer;  // setInterval(function(){})를 담을거임  clearInterval 시발놈 이거때매 알게됨 
 
+    const nameArea = document.getElementById("nameArea"); 
+    let playerName;
+    // 상수 변수 끝
 
     //초기화 로직 
     function initialize(){
@@ -30,6 +36,17 @@ window.onload = function(){
     }
     //초기화 로직 끝
 
+     //사용자 이름 받아오기 로직 
+    function getName(){
+        playerName = prompt("당신의 이름을 적어주세요!",);          // 이름값 받는 prompt
+        
+        while(playerName === "" || playerName === null){  //받은 값이 빈 문자열이거나 null 이면
+            playerName = prompt("좋은 말 할 때 적어주세요^^",);                   // prompt 무한 실행
+        }
+        nameArea.innerHTML = "hello!  " + playerName; 
+    }
+     //사용자 이름 받아오기 로직 끝 
+    
     //버튼 클릭 로직 
     function buttonClick(){
         c += 1;  // 매 회 클릭마다 카운터 변수는 조건없이 바로 +1
@@ -56,83 +73,57 @@ window.onload = function(){
     }
     //버튼 클릭 로직 끝
 
-    //사용자 이름 받아오기 로직 
-    function getName(){
-        const nameArea = document.getElementById("nameArea");                       // 이름 html
-        nameArea.innerHTML = "hello!  " + prompt("당신의 이름을 적어주세요!",);              // 이름값 받는 prompt
-
-        while(nameArea.innerHTML === "hello!  " + "" || nameArea.innerHTML === "hello!  "  + null){  //받은 값이 빈 문자열이거나 null 이면
-            nameArea.innerHTML = "hello!  " + prompt("좋은 말 할 때 적어주세요^^",);                   // prompt 무한 실행
-        }
-    }
-    //사용자 이름 받아오기 로직 끝 
-
     //결과창 로직
-//function result(){
-//    let grade = timerReturn.innerHTML
-//    let gradeReturn;
-//
-//    if(grade >= 7){
-//        gradeReturn = "7초이상"
-//    }
-//    else if((6 <= grade) && (grade < 7)){
-//        gradeReturn = "6초"
-//    }
-//    else if((5 <= grade) && (grade < 6)){
-//        gradeReturn = "5초"
-//    }
-//    else if((4 <= grade) && (grade < 5)){
-//        gradeReturn = "4초"
-//    }
-//    else if((3 <= grade) && (grade < 4)){
-//        gradeReturn = "3초"
-//    }
-//    else if(grade < 3){
-//        gradeReturn = "3초미만"
-//    }
-//    switch(gradeReturn){
-//        case "7초이상" : alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \n당신의 기록... 착잡한 심정입니다...");                   //타이머 멈춘 시점  알람 출력
-//        break;
-//        case "6초" : alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \n저희 집 강아지 온유도 6초는 합니다^^");  
-//        break;
-//        case "5초" : alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \n게이머 평균은 4초대 입니다^^");  
-//        break;
-//        case "4초" : alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \n딱 평균만큼만 하시게요? ^^");  
-//        break;
-//        case "3초" : alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \n인정합니다^^");  
-//        break;
-//        case "3초미만" : alert("당신의 기록은 " + timerReturn.innerHTML + "s 입니다. \nGOD " +nameArea.innerHTML ); 
-//        break;
-//    }
-//}
     function result(){
         let grade = timerReturn.innerHTML;
 
         const baseNotice = "당신의 기록은 " + grade + "s 입니다. \n" 
 
-        function aaa(notice){
+        function rankMsg(notice){
             alert(baseNotice + notice);
         }
-
-        if(grade < 3.0){
-            aaa("GOD " +nameArea.innerHTML)
+        
+        function checkGrade(cut1,cut2){
+            if(cut1 <= grade && grade < cut2){
+                return true;
+            } 
         }
-        else if(Math.floor(grade) === 3.0){
-            aaa("마지못해 인정합니다^^");
+        
+        if(checkGrade(0.0,3.0)){
+            rankMsg("GOD " + playerName);
         }
-        else if(Math.floor(grade) === 4.0){
-            aaa("딱 평균만큼만 하시게요? ^^");  
+        else if(checkGrade(3.0,4.0)){
+            rankMsg("마지못해 인정합니다^^");
         }
-        else if(Math.floor(grade) === 5.0){
-            aaa("게이머 평균은 4초대 입니다^^");
+        else if(checkGrade(4.0,5.0)){
+            rankMsg("딱 평균만큼만 하시게요? ^^");  
         }
-        else if(Math.floor(grade) === 6.0){
-            aaa("저희 집 강아지 온유도 6초는 합니다^^");
+        else if(checkGrade(5.0,6.0)){
+            rankMsg("게이머 평균은 4초대 입니다^^");
+        }
+        else if(checkGrade(6.0,7.0)){
+            rankMsg("저희 집 강아지 온유도 6초는 합니다^^");
         }
         else{  // 나머지 7초 이상
-            aaa("당신의 기록... 착잡한 심정입니다...");
+            rankMsg("당신의 기록... 착잡한 심정입니다...");
         }
-    }   
+    
+       // switch(Math.floor(grade)){
+       //     case 2.0 :
+       //     case 1.0 :
+       //     case 0.0 : rankMsg("GOD " + playerName);
+       //     break;
+       //     case 3.0 : rankMsg("마지못해 인정합니다^^");
+       //     break;
+       //     case 4.0 :  rankMsg("딱 평균만큼만 하시게요? ^^");  
+       //     break;
+       //     case 5.0 :  rankMsg("게이머 평균은 4초대 입니다^^");
+       //     break;
+       //     case 6.0 :  rankMsg("저희 집 강아지 온유도 6초는 합니다^^");
+       //     break;
+       //     default : rankMsg("당신의 기록... 착잡한 심정입니다...");
+       // 
+    }
     //결과창 로직 끝
 
     //실행
